1666cba9550c202560ac2b854465faa64f6b1351
Aky Mac Donald <akymacd@gmail.com>
2022-10-03 16:01:42 -0400

    Added navbar, react socials, framer motion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,75 @@
-import React from 'react'
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
-type Props = {}
+type Props = {};
 
 function Header({}: Props) {
   return (
-    <header>
-      <div>
+    <header className="sticky top-0 p-5 flex items-start justify-around max-w-7x1 mx-auto xl:items-center z-20">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
         {/* Social Icons */}
-      </div>
+        <SocialIcon
+          url="http://linkedin.com/in/akymacdonald"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="http://github.com/solidquartz"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="
+          flex flex-row items-center
+          text-gray-700 cursor-pointer"
+      >
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p
+          className=" 
+            hidden md:inline-flex
+            text-sm text-gray-700"
+        >
+          Get in Touch
+        </p>
+      </motion.div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
