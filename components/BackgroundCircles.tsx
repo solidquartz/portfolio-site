420e10586a79fd11942e9a46b84c822166368011
Aky Mac Donald <akymacd@gmail.com>
2022-10-07 15:10:11 -0400

    Changed image and circle sizes

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -18,7 +18,7 @@ function BackgroundCircles({}: Props) {
         duration: 2.5
       }}
       className="relative flex justify-center items-center">
-      <div className="absolute border border-gray-200 rounded-full h-[300px] w-[300px] mt-52 animate-pulse" />
+      <div className="absolute border border-gray-200 rounded-full h-[400px] w-[400px] mt-52 animate-pulse" />
       <div className="absolute border border-[#B985ED]  opacity-20 rounded-full h-[650px] w-[650px] mt-52 animate-pulse" />
 
       <div />
