9525ca11f385213a3636b0a8b159773c8b4a59b3
Aky Mac Donald <akymacd@gmail.com>
2022-10-21 12:12:05 -0400

    Finished styling skills section with placeholders and added text

--- components/SkillItem.tsx
+++ components/SkillItem.tsx
@@ -6,17 +6,16 @@ type Props = {}
 
 function SkillItem({}: Props) {
   return (
-    <div className="group relative flex flex-column">
-      <Image
-        src={tailwindicon}
-        alt="tailwind css"
-        className="rounded-full border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:-w-32 xl:h-32 filter hover:grayscale transition duration-1000 ease-in-out"
-      />
-
-      <div className="absolute">
-        <div className="flex">
-          <h4>Tailwind</h4>
-        </div>
+    <div className="group relative flex flex-col items-center">
+      <div>
+        <Image
+          src={tailwindicon}
+          alt="tailwind css"
+          className="rounded-full border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:-w-32 xl:h-32 filter hover:grayscale transition duration-1000 ease-in-out"
+        />
+      </div>
+      <div className="flex">
+        <h4>Tailwind</h4>
       </div>
     </div>
   );
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -11,23 +11,26 @@ function Skills({}: Props) {
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.8 }}
       // viewport={{ once: true }}
-      className="h-screen flex relative flex-col text-center md:text-left max-w-[400px] min-h-screen justify-center mx-auto items-center"
+      className="h-screen flex relative flex-col text-center max-w-[400px] min-h-screen justify-center mx-auto items-center"
     >
-      <h3 className="text-4xl font-thin">my tech stack</h3>
+      <div className="space-y-10 px-0">
+        <h3 className="text-4xl font-thin">my tech stack</h3>
+        <p>Thanks to my experience in an intensive web development bootcamp, I'm able to quickly pivot to using new technologies and adopting existing codebases on the fly.</p>
 
-      <div className="grid grid-cols-4 gap-5">
-        <SkillItem />
-        <SkillItem />
-        <SkillItem />
-        <SkillItem />
-        <SkillItem />
-        <SkillItem />
-        <SkillItem />
-        <SkillItem />
-        <SkillItem />
-        <SkillItem />
-        <SkillItem />
-        <SkillItem />
+        <div className="grid grid-cols-4 gap-5">
+          <SkillItem />
+          <SkillItem />
+          <SkillItem />
+          <SkillItem />
+          <SkillItem />
+          <SkillItem />
+          <SkillItem />
+          <SkillItem />
+          <SkillItem />
+          <SkillItem />
+          <SkillItem />
+          <SkillItem />
+        </div>
       </div>
     </motion.div>
   );
